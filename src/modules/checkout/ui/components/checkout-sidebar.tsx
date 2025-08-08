013a376108705629ad5a3c5d02905a0beaed678b
vladimirovUros <95723106+vladimirovUros@users.noreply.github.com>
2025-08-08 16:32:11 +0200

    * checkout page

--- src/modules/checkout/ui/components/checkout-sidebar.tsx
+++ src/modules/checkout/ui/components/checkout-sidebar.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/lib/utils";
-import { CircleXIcon, LoaderIcon } from "lucide-react";
+import { CircleXIcon } from "lucide-react";
 
 interface CheckoutSidebarProps {
   total: number;
